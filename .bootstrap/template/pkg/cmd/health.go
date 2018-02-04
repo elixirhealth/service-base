a7c64c4cb3b3e076e192e1e1213f9b48f8d84f62
Drausin Wulsin <daedalus2718@gmail.com>
2018-02-04 18:18:01 -0500

    add DatastoreClient (#16)

--- .bootstrap/template/pkg/cmd/health.go
+++ .bootstrap/template/pkg/cmd/health.go
@@ -5,7 +5,7 @@ import (
 	"os"
 
 	lserver "github.com/drausin/libri/libri/common/logging"
-	server2 "github.com/drausin/libri/libri/librarian/server"
+	"github.com/drausin/libri/libri/common/parse"
 	"github.com/elxirhealth/service-base/pkg/server"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -30,7 +30,7 @@ func init() {
 }
 
 func getHealthChecker() (server.HealthChecker, error) {
-	addrs, err := server2.ParseAddrs(viper.GetStringSlice(servicenamesFlag))
+	addrs, err := parse.Addrs(viper.GetStringSlice(servicenamesFlag))
 	if err != nil {
 		return nil, err
 	}
---
+++ pkg/server/storage/datastore.go
@@ -0,0 +1,45 @@
+package storage
+
+import (
+	"cloud.google.com/go/datastore"
+	"context"
+)
+
+// DatastoreClient is a interface wrapper for a *datastore.Client to facilitate mocking in tests.
+type DatastoreClient interface {
+	Put(key *datastore.Key, value interface{}) (*datastore.Key, error)
+	Get(key *datastore.Key, dest interface{}) error
+	Delete(keys []*datastore.Key) error
+	Count(ctx context.Context, q *datastore.Query) (int, error)
+	Run(ctx context.Context, q *datastore.Query) *datastore.Iterator
+}
+
+// DatastoreClientImpl implements DatastoreClient.
+type DatastoreClientImpl struct {
+	inner *datastore.Client
+}
+
+// Get wraps datastore.Client.Get(...)
+func (c *DatastoreClientImpl) Get(key *datastore.Key, dest interface{}) error {
+	return c.inner.Get(context.Background(), key, dest)
+}
+
+// Put wraps datastore.Client.Put(...)
+func (c *DatastoreClientImpl) Put(key *datastore.Key, value interface{}) (*datastore.Key, error) {
+	return c.inner.Put(context.Background(), key, value)
+}
+
+// Delete wraps datastore.Client.Delete(...)
+func (c *DatastoreClientImpl) Delete(keys []*datastore.Key) error {
+	return c.inner.DeleteMulti(context.Background(), keys)
+}
+
+// Count wraps datastore.Client.Count(...)
+func (c *DatastoreClientImpl) Count(ctx context.Context, q *datastore.Query) (int, error) {
+	return c.inner.Count(ctx, q)
+}
+
+// Run wraps datastore.Client.Run(...)
+func (c *DatastoreClientImpl) Run(ctx context.Context, q *datastore.Query) *datastore.Iterator {
+	return c.inner.Run(ctx, q)
+}
