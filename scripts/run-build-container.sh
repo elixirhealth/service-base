69dfd1c7752320202072b2c58c3c9150db663cbc
Drausin Wulsin <daedalus2718@gmail.com>
2018-02-15 11:23:33 -0500

    more Postgres & build container tweaks

--- scripts/run-build-container.sh
+++ scripts/run-build-container.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash -eou pipefail
 
+BUILD_USER="builder"
+
 docker run --rm -it \
 	-h elxir-build \
+	-u ${BUILD_USER} \
 	-v "${GOPATH}/src":/go/src \
-	-v ~/.bashrc:/root/.bashrc \
-	-v ~/.gitconfig:/root/.gitconfig \
+	-v ~/.bashrc:"/home/${BUILD_USER}/.bashrc" \
+	-v ~/.gitconfig:"/home/${BUILD_USER}/.gitconfig" \
 	gcr.io/elxir-core-infra/service-base-build:latest
